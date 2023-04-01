e67123f765677f4791c0455526e4e8a802b7294b
kushurox <kushalsai2449@gmail.com>
2023-04-01 22:15:50 +0530

    diff 1

--- src/expressions.rs
+++ src/expressions.rs
@@ -10,15 +10,25 @@
 
 use std::{rc::Rc, ops::{Add, Mul, Sub, Div}, cell::RefCell, fmt::Display};
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
+enum Op {
+    Add,
+    Sub,
+    Mul,
+    Div,
+    Pow,
+    None
+}
+
+#[derive(Debug, Clone, PartialEq)]
 struct ValueData{
     val: f64,
-    op: Option<i8>,
+    op: Op,
     label: Option<&'static str>,
     from: Option<(Value, Value)>
 }
 
-#[derive(Debug, Clone)]
+#[derive(Debug, Clone, PartialEq)]
 pub struct Value{
     ptr: Rc<RefCell<ValueData>>
 }
@@ -26,46 +36,83 @@ pub struct Value{
 impl Value{
     pub fn new(val: f64, label: Option<&'static str>) -> Self{
         Value{
-            ptr:Rc::new(RefCell::new(ValueData { val, op: None, label, from: None }))
+            ptr:Rc::new(RefCell::new(ValueData { val, op: Op::None, label, from: None }))
         }
     }
-    fn new_from(val: f64, op: i8, lhs: Value, rhs: Value) -> Value{
-        Value { ptr: Rc::new(RefCell::new(ValueData { val: val, op: Some(op), label: None, from: Some((lhs, rhs)) })) }
+    fn new_from(val: f64, op: Op, lhs: Value, rhs: Value) -> Value{
+        Value { ptr: Rc::new(RefCell::new(ValueData { val: val, op: op, label: None, from: Some((lhs, rhs)) })) }
+    }
+
+    pub fn set_label(&self, lbl: &'static str){
+        self.ptr.borrow_mut().label = Some(lbl);
+    }
+
+    pub fn get_label(&self) -> &'static str {
+        return self.ptr.borrow().label.unwrap_or("No Name");
     }
 }
 
 impl Add for &Value{
     type Output = Value;
     fn add(self, rhs: Self) -> Self::Output {
-        Value::new_from(self.ptr.borrow().val + rhs.ptr.borrow().val, 0, self.clone(), rhs.clone())
+        Value::new_from(self.ptr.borrow().val + rhs.ptr.borrow().val, Op::Add, self.clone(), rhs.clone())
     }
 }
 
 impl Mul for &Value{
     type Output = Value;
     fn mul(self, rhs: Self) -> Self::Output {
-        Value::new_from(self.ptr.borrow().val * rhs.ptr.borrow().val, 2, self.clone(), rhs.clone())
+        Value::new_from(self.ptr.borrow().val * rhs.ptr.borrow().val, Op::Mul, self.clone(), rhs.clone())
     }
 }
 
 impl Sub for &Value{
     type Output = Value;
     fn sub(self, rhs: Self) -> Self::Output {
-        Value::new_from(self.ptr.borrow().val - rhs.ptr.borrow().val, 1, self.clone(), rhs.clone())
+        Value::new_from(self.ptr.borrow().val - rhs.ptr.borrow().val, Op::Sub, self.clone(), rhs.clone())
     }
 }
 
 impl Div for &Value{
     type Output = Value;
     fn div(self, rhs: Self) -> Self::Output {
-        Value::new_from(self.ptr.borrow().val / rhs.ptr.borrow().val, 3, self.clone(), rhs.clone())
+        Value::new_from(self.ptr.borrow().val / rhs.ptr.borrow().val, Op::Div, self.clone(), rhs.clone())
     }
 }
 
 impl Value{
     pub fn pow(&self, exponent: &Value) -> Self{
-        Value::new_from(self.ptr.borrow().val.powf(exponent.ptr.borrow().val), 4, self.clone(), exponent.clone())
+        Value::new_from(self.ptr.borrow().val.powf(exponent.ptr.borrow().val), Op::Pow, self.clone(), exponent.clone())
     }
+
+    pub fn diff(&self, dep: &Value) -> Value{
+        // differentiates current varaible with respect to any other variable by using chain rule.
+        // finds total derivative
+        let curr = self.ptr.borrow();
+        if self == dep {
+            return Value::new(1.0, None);
+        }
+        if let Some((v1, v2)) = &curr.from {
+            match curr.op {
+                Op::Add => {
+                    &v1.diff(dep) + &v2.diff(dep)
+                },
+                Op::Sub => {
+                    &v1.diff(dep) - &v2.diff(dep)
+                },
+                Op::Mul => {
+                    &(&v1.diff(dep) * &v2) + &(&v2.diff(dep) * &v1)
+                },
+
+                Op::Div => todo!(),
+                Op::Pow => todo!(),
+                Op::None => todo!(),
+            }
+        } else {
+            todo!()
+        }
+    }
+
 }
 
 
--- src/main.rs
+++ src/main.rs
@@ -2,8 +2,6 @@ mod expressions;
 use expressions::Value;
 
 fn main(){
-    let a = Value::new(3.0, Some("hello"));
-    let b = Value::new(2.0, Some("Bye"));
-    let c  = &a + &b;
-    println!("{c}");
+    let a = String::from("Hello");
+    
 }
